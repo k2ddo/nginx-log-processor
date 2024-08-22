63158f3853558decbd09c374f8bdf35b4643a7de
Владислав Автух <vladkiddo@gmail.com>
2024-08-22 18:38:38 +0300

    chore: initialize Django project

---
+++ config/utils.py
@@ -0,0 +1,18 @@
+from os import environ
+from typing import Optional
+
+from .exceptions import MissingEnvVariable
+
+
+def get_env_var(
+    variable_name: str,
+    default: Optional[str] = None,
+    raise_exception: bool = True
+) -> Optional[str]:
+    """Извлекает переменную из среды окружения"""
+    if not (variable := environ.get(variable_name, default)):
+        if raise_exception:
+            raise MissingEnvVariable(f'{variable_name} is missing')
+    
+    print(type(variable))
+    return variable
