d20165f6e68ddc3b3f8e6b9a0f9a2c91c0bd0f48
Владислав Автух <vladkiddo@gmail.com>
2024-08-22 19:35:49 +0300

    Creating LogEntry model

--- logs/models.py
+++ logs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class LogEntry(models.Model):
+    """Модель для хранения записей из лога"""
+    date = models.DateTimeField('Дата')
+    ip_address = models.GenericIPAddressField('IP адрес')
+    user = models.CharField('Имя пользователя', max_length=255)
+    http_method = models.CharField('HTTP метод', max_length=10)
+    request_uri = models.TextField('URI запроса')
+    response_code = models.PositiveSmallIntegerField('Код ответа')
+    response_size = models.IntegerField('Размер ответа')
+
+    def __str__(self):
+        return f"{self.date} - {self.http_method} {self.request_uri} - {self.response_code}"
