083c3bae54fef4b983ad8948857cb6e205e62905
Владислав Автух <vladkiddo@gmail.com>
2024-08-23 16:22:26 +0300

    Added swagger

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # Сторонние библиотеки
     'rest_framework',
     'django_filters',
+    'drf_spectacular',
 
     # Собственные приложения
     'logs',
@@ -141,4 +142,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 50,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
