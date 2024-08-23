2f3682305f19a0dea3e67d87e696b0033016de92
Владислав Автух <vladkiddo@gmail.com>
2024-08-23 14:27:21 +0300

    feat(admin): add log entry filters

--- logs/admin.py
+++ logs/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import LogEntry
 
 
-admin.site.register(LogEntry)
+class Filter(admin.ModelAdmin):
+    list_display = ['date', 'http_method', 'request_uri', 'response_code',]
+    list_filter = ['http_method', 'request_uri', 'response_code',]
+
+
+admin.site.register(LogEntry, Filter)
